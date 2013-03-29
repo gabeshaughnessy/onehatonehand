a8a2859bd51cc30330080321268acc3e7c13b961
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-03-28 18:10:05 -0700

    maintenance splash page

---
+++ wp-content/themes/1h1h/503.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+<head profile="http://gmpg.org/xfn/11">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="viewport" content="">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="">
+<title> One Hat One Hand</title>
+<link rel="pingback" href="http://imagination-station.local/1h1h/xmlrpc.php" />
+<link rel="apple-touch-icon" href="http://imagination-station.local/1h1h/wp-content/themes/1h1h/images/iOs_114px.jpg" />
+
+<link rel="apple-touch-icon" sizes="72x72" href="http://imagination-station.local/1h1h/wp-content/themes/1h1h/images/iOs_75px.jpg" />
+
+<link rel="apple-touch-icon" sizes="114x114" href="http://imagination-station.local/1h1h/wp-content/themes/1h1h/images/iOs_114px.jpg" />
+<link rel="stylesheet" href="http://imagination-station.local/1h1h/wp-content/themes/1h1h/jquery-ui-1.8.21.custom.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="http://imagination-station.local/1h1h/wp-content/themes/1h1h/style.css" type="text/css" media="screen" />
+<!-- Google Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Fredericka+the+Great' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Parisienne' rel='stylesheet' type='text/css'>
+<!-- End google fonts -->
+
+<script type="text/javascript">
+//Creates a global variable with the theme directory for use in js functions
+var templateDir = "http://imagination-station.local/1h1h/wp-content/themes/1h1h";
+</script>
+</head>
+
+<body class="home blog" data-spy="scroll" >
+<div id="fixed_bg"></div>
+<div id="wrapper" class="no-fade">
+<div id="landing" class="section active">
+	<div id="landing-wrapper" class="wrapper">
+		<div class="post-box">
+			<div id="main-hat" class="hat"><span class="fredericka">San Francisco, CA</span></div>
+			<div id="main-logo">One Hat One Hand</div>
+			<div id="construction-cta" class="cta muli"><div class="thick-line line"></div><p>Our Site is Under Construction.</p><div class="thick-line line"></div></div>
+			
+			<div id="checkback-cta" class="cta muli"><div class="thick-line line"></div><p>Check Back Soon!</p><div class="thick-line line"></div></div>
+			<div id="login-link">
+				<span class="thin-line line"></span><a href="login" title="login to the site" class="parisienne">login</a><span class="thin-line line"></span>
+			</div>
+			<div id="intro-description">
+				<p class="muli">One Hat One Hand is a set design and fabrication shop owned and operated in San Francisco, CA. We specialize in building the impossible and we are confident if you can imagine it, we can build it.
+				</p>
+			</div>
+			<div id="main-hand" class="hand"><span class="fredericka">Imagine Design Build</div>
+		</div><!--end post box -->
+	</div><!-- end landing-wrapper -->
+</div><!-- end landing section -->
+<div class="clear"></div>
+
+</div> <!-- // wrapper -->
+</body>
+</html>
