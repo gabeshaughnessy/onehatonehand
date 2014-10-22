1920d7974fd277ff36141cabc652dd6e280194e6
Alex <alex@of-6.com>
2014-10-22 00:10:25 -0700

    remove login link

--- wp-content/themes/1h1h/content-landing.php
+++ wp-content/themes/1h1h/content-landing.php
@@ -9,11 +9,13 @@
 			<div id="scroll-cta" class="cta muli"><div class="thick-line line"></div><p>scroll down to navigate the site</p><div class="thick-line line"></div></div>
 			
 			<div id="keys-cta" class="cta muli hide-for-touch"><div class="thick-line line"></div><span class="keyboard_arrows"></span><p>or use the keyboard </p><div class="thick-line line"></div></div>
-			<div id="login-link">
-				<span class="thin-line line"></span><a href="login" title="login to the site" class="parisienne">login</a><span class="thin-line line"></span>
-			</div>
+			
+			<br>
+			
 			<p class="fredericka centered tagline">A Collective Approach to Design and Fabrication</p>
+			
 			<div id="main-hand" class="hand"></div>
+			
 		</div><!--end post box -->
 	</div><!-- end landing-wrapper -->
 </div><!-- end landing section -->
