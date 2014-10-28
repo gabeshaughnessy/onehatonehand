d6d49733bdc0f87abeaeac99a9fbd5f91044d284
Alex <alex@of-6.com>
2014-10-28 16:31:41 -0700

    update signup form to use mailchimp form

--- wp-content/themes/1h1h/content-media.php
+++ wp-content/themes/1h1h/content-media.php
@@ -19,11 +19,27 @@
                 <h3 class="subtitle fredericka centered"><?php the_field('newsletter_title'); ?></h3>
 
                 <?php the_field('newsletter_copy'); ?>
-
-                <form class="newsletter-form" action="">
-                  <input type="email" class="input-email" placeholder="Email">
-                  <input type="submit" class="submit" value="Send">
-                </form>
+                
+                <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+                <div id="mc_embed_signup">
+                  <form action="//onehatonehand.us6.list-manage.com/subscribe/post?u=0584c4635ebc579c83f7cad10&amp;id=4ba2aed7b9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="newsletter-form validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+                      <div style="position: absolute; left: -5000px;">
+                        <input type="text" name="b_0584c4635ebc579c83f7cad10_4ba2aed7b9" tabindex="-1" value="">
+                      </div>
+                      <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                      <input type="submit" value="Signup" name="subscribe" id="mc-embedded-subscribe" class="submit">
+                      <div id="mce-responses" class="clear">
+                        <div class="response" id="mce-error-response" style="display:none"></div>
+                        <div class="response" id="mce-success-response" style="display:none"></div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+                <script type="text/javascript">
+                  (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[3]='MMERGE3';ftypes[3]='text';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
+              </script>
 
               </div>
               
